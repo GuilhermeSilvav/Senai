5cbb2259d00cfa76f7ff12aab28f57f8d3e64000
GuilhermeSilvav <127443339+GuilhermeSilvav@users.noreply.github.com>
2023-05-10 22:00:47 -0300

    Add files via upload

---
+++ prova_10_05.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author guilherme_s_velho
+ */
+public class prova_10_05 {
+    
+}
